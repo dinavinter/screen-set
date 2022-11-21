707ae32f8b5dc43c642c7ad8e556dc0730b2ab3b
dinavinter <dina.vinter@sap.com>
2022-11-21 06:33:41 +0200

    no local storage

--- src/auth/AuthProvider.tsx
+++ src/auth/AuthProvider.tsx
@@ -1,15 +1,15 @@
 import React, {useEffect, useState, createContext, useContext} from "react";
 import {GigyaContext, GigyaSdk, useGigya} from "../gigya/provider";
 import {authMachine, AuthService, AuthMachineContext, AuthMachine} from "../machines/authMachine";
-import {useInterpretWithLocalStorage} from "../machines/withLocalStorage";
 import {withGigya} from "../machines/withGigya";
+import {useInterpret} from "@xstate/react";
 
 export const AuthContext = createContext<AuthService>({} as AuthService);
 
 export function AuthProvider({ children}:React.PropsWithChildren) {
 
     const getMachine=():AuthMachine => withGigya(authMachine);
-    const authService= useInterpretWithLocalStorage(getMachine);
+    const authService= useInterpret(getMachine);
  
         return  <AuthContext.Provider value={authService}>
             {children}
